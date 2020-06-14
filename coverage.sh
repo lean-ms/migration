7e4d852e5c955ef465c74b6da23ceccf41df01e4
Paulo Soares <phsoares.ita@gmail.com>
2020-06-14 09:11:35 -0300

    adding logs to coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,14 @@
+echo "[INFO] Downloading cc reporter"
 export CC_TEST_REPORTER_URL=https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64
 export CC_TEST_REPORTER_ID=584dbd5c892f225f70261e9af11ab6dd5e363bb3a3b1d02109993300faee8ecc
 curl -L $CC_TEST_REPORTER_URL > /tmp/cc-test-reporter
 chmod +x /tmp/cc-test-reporter
+
+echo "[INFO] Setting up cc reporter"
 /tmp/cc-test-reporter before-build
+
+echo "[INFO] Running tests"
 go test -coverprofile c.out ./...
-/tmp/cc-test-reporter after-build --exit-code $?
+
+echo "[INFO] Sending stats of previous successful tests"
+/tmp/cc-test-reporter after-build --exit-code 0
